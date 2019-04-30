daf6e09d25680992fc08734b9480fa51d5c0a9f4
Mariaum <me@mariaum.com>
2019-04-30 13:11:53 -0300

    Fix build script.

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-git submodule update --init && ./scripts/applyPatches.sh
+git submodule update --init --recursive && ./scripts/applyPatches.sh
 
 if [ "$1" == "--jar" ]; then
      mvn clean package
